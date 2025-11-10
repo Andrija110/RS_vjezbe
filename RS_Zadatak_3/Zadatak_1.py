3bd63ad012c7d25744ee36528137d4118587c475
Andrija <andrija.krizan@gmail.com>
2025-11-10 20:52:12 +0100

    Zadaci_1_i_2

---
+++ RS_Zadatak_3/Zadatak_1.py
@@ -0,0 +1,20 @@
+
+#%%
+# Kvadriranje broja
+Kvadriraj = lambda x: x ** 2
+
+#%%
+# Zbroji pa kvadriraj
+Zbroji_i_kvadriraj = lambda a, b: (a + b) ** 2
+
+#%%
+#Kvadriraj duljinu niza
+Kvadriraj_duljinu_niza = lambda niz: len(niz) ** 2
+
+#%%
+#Pomnoži vrijednost s 5 pa potenciraj na x
+Pomnozi_i_potenciraj = lambda x: (x * 5) ** x
+
+#%%
+#Vrati True ako je broj paran, inače vrati None
+Vrati_True_ako_je_paran = lambda x: x % 2 == 0
